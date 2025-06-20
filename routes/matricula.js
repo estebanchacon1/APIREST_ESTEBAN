be3b6835e689c8258ccf56e4369398403426c6ef
Esteban <estebanluischacon47@gmail.com>
2025-06-20 15:09:06 -0500

    cambio

--- routes/matricula.js
+++ routes/matricula.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Post = require('../models/Matricula'); // importo el modelo
+const Matricula = require('../models/Matricula'); // importo el modelo
 
 router.get('/', async (req, res) => {
     try {
